ca209fa1f400efdb29545f686bfb7acd0e27f737
vinothrwins <vinothrwins@gmail.com>
2025-12-07 13:51:37 +0000

    title added

--- src/main/java/com/vts/pages/CartPage.java
+++ src/main/java/com/vts/pages/CartPage.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+// Code updated by Vinoth R
 /**
  * Page Object Model for the Shopping Cart page.
  * This version uses PageFactory.
